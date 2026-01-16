fe1ae69b84a98de9ea2577d0b69a9c8e6c21afc4
ulazimo <ulazimo@users.noreply.github.com>
2026-01-16 15:49:38 +0000

    deploy: 9dbd5b513f94e376874aa6c51674e8154c9b9e65

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC2RSC711FagoP5XsODIFfLMMZUX6Up-xM",
+  authDomain: "prototypes-7691b.firebaseapp.com",
+  projectId: "prototypes-7691b",
+  storageBucket: "prototypes-7691b.firebasestorage.app",
+  messagingSenderId: "263652926534",
+  appId: "1:263652926534:web:9af3c4ce63aab78f0f9647",
+  measurementId: "G-1X91H99N2G"
+};
